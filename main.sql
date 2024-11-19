69cb761e13a671eac16f137630c0c4121e4d2835
aserturik <alex.hernandez27@uptc.edu.co>
2024-11-19 16:54:36 -0500

    tablas de datamodeler

--- main.sql
+++ main.sql
@@ -59,21 +59,144 @@ ALTER SESSION SET NLS_TERRITORY=America;
 --
 
 -- @__SUB__CWD__/human_resources/hr_cr
-CREATE TABLE clientes
-    (   cliente_id      NUMBER(10)
-     ,  names
-        last_names
-phone_number
-email
-direction
-register_date
+
+
+CREATE TABLE clientes (
+    id_cliente       NUMBER(10) NOT NULL,
+    nombres          VARCHAR2(30),
+    apellidos        VARCHAR2(30),
+    tel�fono         VARCHAR2(15),
+    email            VARCHAR2(100),
+    direccion        VARCHAR2(100),
+    "FECHA REGISTRO" DATE
 );
 
-CREATE TABLE CLIENTES (
-    cliente_id NUMBER(7),
-    nombres VARCHAR2(100),
+ALTER TABLE clientes ADD CONSTRAINT clientes_pk PRIMARY KEY ( id_cliente );
 
+CREATE TABLE detalles_de_ordenes (
+    id_detalle              NUMBER(10) NOT NULL,
+    id_orden                NUMBER(10) NOT NULL,
+    detalles_orden_producto VARCHAR2(500),
+    id_producto_material    NUMBER(10) NOT NULL
 );
+
+ALTER TABLE detalles_de_ordenes ADD CONSTRAINT detalles_de_ordenes_pk PRIMARY KEY ( id_detalle );
+
+CREATE TABLE empleados (
+    id_empleado           NUMBER(10) NOT NULL,
+    nombres               VARCHAR2(30),
+    apellidos             VARCHAR2(30),
+    telefono              VARCHAR2(15),
+    cedula                NUMBER(20),
+    salario               NUMBER(15, 2),
+    fecha_de_contrataci�n DATE,
+    email                 VARCHAR2(100),
+    gerente_id            NUMBER(10),
+    "PUESTOS_ID-PUESTO"   NUMBER(10) NOT NULL
+);
+
+CREATE UNIQUE INDEX empleados_tel_idx ON
+    empleados (
+        telefono
+    ASC );
+
+ALTER TABLE empleados ADD CONSTRAINT empleados_pk PRIMARY KEY ( id_empleado );
+
+CREATE TABLE materiales (
+    id_material              NUMBER(10) NOT NULL,
+    nombre_material          VARCHAR2(100),
+    descripcion              VARCHAR2(500),
+    costo_unitario           NUMBER(10, 2),
+    proveedores_id_proveedor NUMBER(10)
+);
+
+ALTER TABLE materiales ADD CONSTRAINT materiales_pk PRIMARY KEY ( id_material );
+
+CREATE TABLE ordenes (
+    id_orden      NUMBER(10) NOT NULL,
+    fecha_orden   DATE,
+    fecha_entrega DATE,
+    estado        VARCHAR2(50),
+    total         NUMBER(15, 2),
+    id_cliente    NUMBER(10) NOT NULL,
+    id_empleado   NUMBER(10) NOT NULL
+);
+
+ALTER TABLE ordenes ADD CONSTRAINT ordenes_pk PRIMARY KEY ( id_orden );
+
+CREATE TABLE producto_material (
+    id_producto_material   NUMBER(10) NOT NULL,
+    stock                  NUMBER(10),
+    materiales_id_material NUMBER(10) NOT NULL,
+    productos_id_producto  NUMBER(10) NOT NULL,
+    precio_unitario        NUMBER(6)
+);
+
+ALTER TABLE producto_material ADD CONSTRAINT producto_material_pk PRIMARY KEY ( id_producto_material );
+
+CREATE TABLE productos (
+    id_producto NUMBER(10) NOT NULL,
+    nombre      VARCHAR2(100),
+    descripcion VARCHAR2(500)
+);
+
+ALTER TABLE productos ADD CONSTRAINT productos_pk PRIMARY KEY ( id_producto );
+
+CREATE TABLE proveedores (
+    id_proveedor NUMBER(10) NOT NULL,
+    nombres      VARCHAR2(30),
+    apellidos    VARCHAR2(30),
+    direccion    VARCHAR2(100),
+    telefono     VARCHAR2(15),
+    email        VARCHAR2(100)
+);
+
+ALTER TABLE proveedores ADD CONSTRAINT proveedores_pk PRIMARY KEY ( id_proveedor );
+
+CREATE TABLE puestos (
+    "ID-PUESTO"   NUMBER(10) NOT NULL,
+    nombre_puesto VARCHAR2(15)
+);
+
+ALTER TABLE puestos ADD CONSTRAINT puestos_pk PRIMARY KEY ( "ID-PUESTO" );
+
+ALTER TABLE detalles_de_ordenes
+    ADD CONSTRAINT detalles_de_ordenes_ordenes_fk FOREIGN KEY ( id_orden )
+        REFERENCES ordenes ( id_orden );
+
+--  ERROR: FK name length exceeds maximum allowed length(30) 
+ALTER TABLE detalles_de_ordenes
+    ADD CONSTRAINT detalles_de_ordenes_producto_material_fk FOREIGN KEY ( id_producto_material )
+        REFERENCES producto_material ( id_producto_material );
+
+ALTER TABLE empleados
+    ADD CONSTRAINT empleados_empleados_fk FOREIGN KEY ( gerente_id )
+        REFERENCES empleados ( id_empleado );
+
+ALTER TABLE empleados
+    ADD CONSTRAINT empleados_puestos_fk FOREIGN KEY ( "PUESTOS_ID-PUESTO" )
+        REFERENCES puestos ( "ID-PUESTO" );
+
+ALTER TABLE materiales
+    ADD CONSTRAINT materiales_proveedores_fk FOREIGN KEY ( proveedores_id_proveedor )
+        REFERENCES proveedores ( id_proveedor );
+
+ALTER TABLE ordenes
+    ADD CONSTRAINT ordenes_clientes_fk FOREIGN KEY ( id_cliente )
+        REFERENCES clientes ( id_cliente );
+
+ALTER TABLE ordenes
+    ADD CONSTRAINT ordenes_empleados_fk FOREIGN KEY ( id_empleado )
+        REFERENCES empleados ( id_empleado );
+
+--  ERROR: FK name length exceeds maximum allowed length(30) 
+ALTER TABLE producto_material
+    ADD CONSTRAINT producto_material_materiales_fk FOREIGN KEY ( materiales_id_material )
+        REFERENCES materiales ( id_material );
+
+ALTER TABLE producto_material
+    ADD CONSTRAINT producto_material_productos_fk FOREIGN KEY ( productos_id_producto )
+        REFERENCES productos ( id_producto );
 -- 
 -- populate tables
 --
